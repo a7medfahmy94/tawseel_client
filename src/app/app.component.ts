ed283a65f4948618c87d80366ee029e763ea2c5c
Ahmed Fahmy <a7med.fahmy94@gmail.com>
2016-12-07 22:18:33 +0200

    replace the project with client project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
-  selector   : 'app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  logs: any;
+  cost: any;
+  constructor(af: AngularFire) {
+    let sub = af.database.list('/trips').subscribe(data => {
+      for (var trip of data) {
+        if (trip.state == 'started' && trip.logs) {
+          this.logs = af.database.list('/trips/'+trip.$key+'/logs');
+          af.database.object('/trips/'+trip.$key+'/cost').subscribe(data => {
+            console.log(data);
+            if (data.$value) {
+              this.cost = data.$value;
+            }
+          })
+          sub.unsubscribe();
+        }
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import {NgModule} from '@angular/core'
-import {RouterModule} from "@angular/router";
-import {rootRouterConfig} from "./app.routes";
-import {AppComponent} from "./app.component";
-import {GithubService} from "./github/shared/github.service";
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {HttpModule} from "@angular/http";
-import {AboutComponent} from './about/about.component';
-import {HomeComponent} from './home/home.component';
-import {RepoBrowserComponent} from './github/repo-browser/repo-browser.component';
-import {RepoListComponent} from './github/repo-list/repo-list.component';
-import {RepoDetailComponent} from './github/repo-detail/repo-detail.component';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDn_NMUsa9sVsYm19ApwK2U8juSC1PYLfM',
+  authDomain: 'tawseel-37034.firebaseapp.com',
+  databaseURL: 'https://tawseel-37034.firebaseio.com',
+  storageBucket: 'tawseel-37034.appspot.com'
+};
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, RepoBrowserComponent, RepoListComponent, RepoDetailComponent, HomeComponent],
-  imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
-  providers   : [GithubService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
-  bootstrap   : [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule { }
